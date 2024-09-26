fa57045005b116c47874a7adec5c53b1970deea2
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-09-26 15:31:13 +0530

    commit

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,17 +3,15 @@ const app = express();
 const Task = require("../models/Task");
 
 // GET REQUEST
-
 app.get("/api/tasks", (req, res) => {
   Task.find()
-    .then((task) => {
-      res.json(task);
+    .then((tasks) => {
+      res.json(tasks);
     })
-    .catch((err) => res.status(400).json(`Error ${err}`));
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 // POST REQUEST
-
 app.post("/api/tasks", (req, res) => {
   const { title, description, completed } = req.body;
 
@@ -26,40 +24,39 @@ app.post("/api/tasks", (req, res) => {
   newTask
     .save()
     .then(() => res.json("Task added!"))
-    .catch((err) => res.status(400).json(`Error ${err}`));
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 // GET task based on 'id'
-
 app.get("/api/tasks/:id", (req, res) => {
   Task.findById(req.params.id)
     .then((task) => res.json(task))
-    .catch((err) => res.status(400).json(`Error ${err}`));
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 // PUT which is used for 'UPDATING' the task
-
 app.put("/api/tasks/:id", (req, res) => {
   Task.findById(req.params.id)
     .then((task) => {
-      (task.title = req.body.title),
-        (task.description = req.body.description),
-        (task.completed = req.body.completed);
+      task.title = req.body.title;
+      task.description = req.body.description;
+      task.completed = req.body.completed;
 
       task
         .save()
         .then(() => res.json("Task Updated!"))
-        .catch((err) => res.status(400).json(`Error ${err}`));
+        .catch((err) => res.status(400).json(`Error: ${err}`));
     })
-    .catch((err) => res.status(400).json(`Error ${err}`));
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
 // DELETE a task by 'id'
-
 app.delete("/api/tasks/:id", (req, res) => {
   Task.findByIdAndDelete(req.params.id)
     .then(() => {
       res.json("Task Deleted!");
     })
-    .catch((err) => res.status(400).json(`Error ${err}`));
+    .catch((err) => res.status(400).json(`Error: ${err}`));
 });
+
+module.exports = app;
