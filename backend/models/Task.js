70d5cc817e4b52c426afb0c77075291b7b990560
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-09-27 21:39:46 +0530

    updated the schema

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -2,21 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+// Task schema
 const taskSchema = new Schema(
   {
-    title: { type: String, required: true },
-    description: {
-      type: String,
-    },
-    completed: {
-      type: Boolean,
-      default: false,
-    },
+    title: { type: String, required: true }, 
+    description: { type: String }, 
+    completed: { type: Boolean, default: false } 
   },
   {
-    timestamps: true,
+    timestamps: true, 
   }
 );
 
 const Task = mongoose.model("Task", taskSchema);
+
 module.exports = Task;
