6467deb0f66b6eaa5714e5ffd677311cd069dcb9
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-09-29 11:06:48 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const cors = require("cors");
+require("dotenev").config();
 const taskRoutes = require("./routes/tasks");
 
 const app = express();
@@ -9,21 +9,21 @@ const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
 
 // Middlewares
-app.use(cors());
-app.use(express.json());
 
-// Connecting to Database
+app.use(cors());
+app.use(express.body());
+
+// Connecting to MongoDB
 
 mongoose.connect(uri);
 
 const connection = mongoose.connection;
-
 connection.once("open", () => {
-  "Established connection to Database";
+  console.log("Established connection to the Database");
 });
 
 app.use("/api/tasks", taskRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port : ${port}`);
 });
