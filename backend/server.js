6607c018dcfd5c2e122df18fbed0c49b1ceeaeb1
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-10-01 23:43:03 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,29 @@ require("dotenv").config();
 const taskRoutes = require("./routes/tasks");
 
 const app = express();
-const port = process.env.REACT_PORT || 5001;
-const uri = process.env.REACT_MONGODB_URI;
+
+const port = process.env.PORT || 5001;
+const uri = process.env.MONGODB_URI; 
 
 // Middlewares
-
 app.use(cors());
 app.use(express.json());
 
 // Connecting to MongoDB
-
-mongoose.connect(uri);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("Established connection to the Database");
 });
 
+// Routes
 app.use("/api/tasks", taskRoutes);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
